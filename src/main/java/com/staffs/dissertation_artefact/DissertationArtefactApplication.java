1450146b5319393451b3a566f477be68fea177ac
Bashlaw <44432200+Bashlaw@users.noreply.github.com>
2024-04-07 17:37:06 +0300

    first commit

---
+++ src/main/java/com/staffs/dissertation_artefact/DissertationArtefactApplication.java
@@ -0,0 +1,13 @@
+package com.staffs.dissertation_artefact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DissertationArtefactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DissertationArtefactApplication.class , args);
+    }
+
+}
---
+++ src/test/java/com/staffs/dissertation_artefact/DissertationArtefactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.staffs.dissertation_artefact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DissertationArtefactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
